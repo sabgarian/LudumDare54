66b7d40de236acff17ab94a5f6a26f1fe4577726
BrianFellDownTheWell <brianlyang@outlook.com>
2023-09-30 14:05:53 -0700

    Added player object and player movement script with WASD and arrow keys for controls.

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0f, velocity * Time.deltaTime, 0f);  
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0f, -1f * velocity * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1f * velocity * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(velocity * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
