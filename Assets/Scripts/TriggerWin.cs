2cffd1418e5a68649f3b5097e1037c9111b0840a
BrianFellDownTheWell <brianlyang@outlook.com>
2023-10-01 14:42:25 -0700

    Added goalpost that will eventually trigger a win condition if touched

---
+++ Assets/Scripts/TriggerWin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWin : MonoBehaviour
+{
+    GameObject VictoryScr;
+
+    void Start()
+    {
+        VictoryScr.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            VictoryScr.SetActive(true);
+        }
+    }
+}
