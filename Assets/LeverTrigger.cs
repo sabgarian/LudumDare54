a1f7db5ae34b9a902874fc5db2af9c062e9e0495
BrianFellDownTheWell <brianlyang@outlook.com>
2023-10-01 16:32:50 -0700

    Added lever triggers that should cause an image to pop up if the player gets within range.

---
+++ Assets/LeverTrigger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeverTrigger : MonoBehaviour
+{
+    //private bool displayKey;
+    SpriteRenderer key;
+
+    void Start()
+    {
+        key = GetComponent<SpriteRenderer>();
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            key.enabled = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            key.enabled = false;
+        }
+    }
+}
